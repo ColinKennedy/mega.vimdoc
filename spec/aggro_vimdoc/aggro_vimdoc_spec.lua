97325b35d31e345c9e74d1ef2deb07cd0ff05dce
Colin Kennedy <colinvfx@gmail.com>
2024-12-06 20:58:50 -0800

    Added more test code

--- spec/aggro_vimdoc/aggro_vimdoc_spec.lua
+++ spec/aggro_vimdoc/aggro_vimdoc_spec.lua
@@ -5,13 +5,44 @@
 
 local vimdoc = require("aggro.vimdoc")
 
---- Read text from file on-disk, `path`.
+---@type string[]
+local _DIRECTORIES_TO_DELETE = {}
+
+local _ORIGINAL_VIM_NOTIFY = vim.notify
+
+--- Get a sub-section copy of `table_` as a new table.
 ---
----@param path string An absolute path on-disk to read.
----@return string # The found text.
+---@param table_ table<any, any>
+---    A list / array / dictionary / sequence to copy + reduce.
+---@param first? number
+---    The start index to use. This value is **inclusive** (the given index
+---    will be returned). Uses `table_`'s first index if not provided.
+---@param last? number
+---    The end index to use. This value is **inclusive** (the given index will
+---    be returned). Uses every index to the end of `table_`' if not provided.
+---@param step? number
+---    The step size between elements in the slice. Defaults to 1 if not provided.
+---@return table<any, any>
+---    The subset of `table_`.
 ---
-local function _get_text(path)
-    return vim.fn.join(vim.fn.readfile(path), "\n")
+local function _get_slice(table_, first, last, step)
+    local sliced = {}
+
+    for i = first or 1, last or #table_, step or 1 do
+        sliced[#sliced + 1] = table_[i]
+    end
+
+    return sliced
+end
+
+--- Stop mini.doc from printing during unittests.
+local function _silence_mini_doc()
+    vim.notify = function(...) end
+end
+
+--- Revert any mocks before unittests were ran.
+local function _reset_mini_doc()
+    vim.notify = _ORIGINAL_VIM_NOTIFY
 end
 
 --- Make a file ending in `suffix`.
@@ -21,7 +52,9 @@ end
 ---
 local function _make_temporary_file(suffix)
     local path = vim.fn.tempname() .. suffix
-    vim.fn.mkdir(vim.fs.dirname(path), "p")
+    local directory = vim.fs.dirname(path)
+    table.insert(_DIRECTORIES_TO_DELETE, directory)
+    vim.fn.mkdir(directory, "p")
 
     return path
 end
@@ -41,31 +74,79 @@ local function _run_test(source_text, expected)
     vim.fn.writefile(vim.fn.split(source_text, "\n"), source)
     vimdoc.make_documentation_files({{source=source, destination=destination}})
 
-    local found = _get_text(destination)
+    -- NOTE: We ignore the last few lines because they are auto-generated.
+    local raw = vim.fn.readfile(destination)
+    local found = vim.fn.join(_get_slice(raw, 1, math.max(#raw - 3, 1)), "\n")
 
     assert.equal(expected, found)
 end
 
---- TODO: write this
----
---- - Internal linking for @param
----     - Custom types, too
---- - Classes (metatables)
----
----
+--- Reset all dependencies and clean up and temporary files / directories.
+local function _after_each()
+    _reset_mini_doc()
+
+    for _, directory in ipairs(_DIRECTORIES_TO_DELETE) do
+        vim.fn.delete(directory)
+    end
+
+    _DIRECTORIES_TO_DELETE = {}
+end
 
 describe("@class", function()
-    it("works with a contiguous @class definition - 001 - No descriptions", function()
+    before_each(_silence_mini_doc)
+    after_each(_after_each)
+
+    it("works with a contiguous @class definition - 001 - no descriptions", function()
         _run_test(
             [[
-            ---@class Foo
-            ---@field bar string
-            ---@field fizz number
-            ---@field buzz integer
+---@class Foo
+---@field bar string
+---@field fizz number
+---@field buzz integer
             ]],
             [[
-            asdfasdfasfdasdf
-            ]]
+==============================================================================
+------------------------------------------------------------------------------
+*Foo*
+
+Fields ~
+    {bar} `(string)`
+    {fizz} `(number)`
+    {buzz} `(integer)`
+]]
+        )
+    end)
+
+    it("works with a contiguous @class definition - 002 - with descriptions", function()
+        _run_test(
+            [[
+---@class Foo
+---    And some text here.
+---@field bar string
+---    More information.
+---@field fizz number
+---    Lines and lines.
+---    with more lines here
+---    that span extra lines.
+---@field buzz integer
+---    Etc etc.
+            ]],
+            [[
+==============================================================================
+------------------------------------------------------------------------------
+*Foo*
+   And some text here.
+
+Fields ~
+    {bar} `(string)`
+       More information.
+    {fizz} `(number)`
+       Lines and lines.
+       with more lines here
+       that span extra lines.
+    {buzz} `(integer)`
+       Etc etc.
+]]
         )
     end)
 end)
